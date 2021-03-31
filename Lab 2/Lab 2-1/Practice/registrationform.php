1b266402cf163b5851046e2bb1ea9ef1a8e3b0fd
NXTung1102000 <nxtung1102000@gmail.com>
2021-03-31 20:41:06 +0700

    add practice lab 2.1

---
+++ Lab 2/Lab 2-1/Practice/registrationform.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="http://fit.hut.edu.vn/~trangntt/courses/wp"
+              method="post">
+                  <table>
+                        Đăng ký để tham gia:
+			<tr>
+				<td colspan="2">Form dang ky</td>
+			</tr>	
+			<tr>
+				<td>Ten dang nhap :</td>
+				<td><input type="text" id="username" name="username"></td>
+			</tr>
+			<tr>
+				<td>Mat khau :</td>
+				<td><input type="password" id="pass" name="pass"></td>
+			</tr>
+			<tr>
+				<td>Ho Ten :</td>
+				<td><input type="text" id="name" name="name"></td>
+			</tr>
+			<tr>
+				<td>Email :</td>
+				<td><input type="text" id="email" name="email"></td>
+			</tr>
+                        <tr>
+				<td>So dien thoai :</td>
+				<td><input type="text" id="numberphone" name="numberphone"></td>
+			</tr>
+			<tr>
+				<td colspan="2" align="left">
+                                    <input type="submit" name="submit" value="Dang ky">
+                                    <input type="reset" name="reset" value="Reset">
+                                </td>
+			</tr>
+
+		</table>
+        </form>
+    </body>
+</html>
