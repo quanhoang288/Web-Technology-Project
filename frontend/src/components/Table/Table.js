3fcb3f2d2ac2409a8e90f5a5f5d0a501a2278450
thangnd3629 <hsgsdominate@gnail.com>
2021-04-14 23:24:01 +0700

    'UpdateTable'

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -3,111 +3,107 @@ import './Table.css'
 import React, { Component } from 'react'
 
 export class Table extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props)
         this.state = {
-            data:this.props.data,
-            query: '',
-            query_field: []
+            data: this.props.data,
+            query : '',
+            query_field: [],
+
         }
     }
-    
-    componentDidMount() {
-        
-        this.setState({ data: this.props.data })
 
+    componentDidMount() {
+
+        this.setState({ data: this.props.data })
         
+        //init query field 
+        const columns = this.props.data[0] && Object.keys(this.props.data[0])
+        const query_field = columns ? columns : []
+        this.setState({query_field:query_field})
+        
+
+
     }
-    
+
     rowClick = (row_data) => {
         console.log(row_data)
     }
-    
-    
-    searchFieldChangeHandler = (field_index) => {
-        const query_field = this.state.query_field
-        
-        this.setState({query_field:query_field})
-    }
-    
+
+
+
     queryHandler = (e) => {
 
-        this.setState({ query: e.target.value })
-
-        if (e.target.value === '') {
-            this.setState({ data: this.props.data });
-
-        }
-        else {
-            //perform searching
-            const matchingIndex = []
-            const row_data = this.props.data.map((elm, idx) => {
-                return (Object.values(elm))
-            })
-
-            row_data.forEach((row, idx) => {
-                row.forEach((field, idx) => {
-                    let match = String(field).indexOf(this.state.query)
-                    if (match > -1) {
-                        matchingIndex.push(idx)
-                        console.log(matchingIndex)
-                    }
-                }
-                )
-            }
-
-            )
-            this.setState({
-                'data': this.props.data.filter((_, idx) => {
-                    return matchingIndex.indexOf(idx) !== -1
-                })
-            })
-        }
+        this.setState({query:e.target.value})
     }
+    filterData = (data) => {
+        const matchingIndex = []
+        
+        data.forEach((row, idx) => {
+            let flag = true
+            this.state.query_field.forEach((field) => {
+                let match = String(row[field]).toLowerCase().indexOf(this.state.query.toLowerCase())
+                if (match > -1 && flag) {
+                    matchingIndex.push(idx)
+                    flag = false
+                }
+            })
+        }
+        )
+        return this.props.data.filter((_, idx) => {
+            return matchingIndex.indexOf(idx) !== -1
+        })
 
+    }
     render() {
-        const data = this.state.data
-        const columns = data[0] && Object.keys(data[0])
-        return (
-            <React.Fragment>
 
+        const data = this.filterData(this.props.data)
+        const columns = this.props.data[0] && Object.keys(this.props.data[0])
+        return (
+
+
+            <React.Fragment>
+                
 
                 <div>
-                    <input onChange={this.queryHandler} value={this.state.query}></input>
+                    <input onChange={this.queryHandler}></input>
                 </div>
 
 
-                <div class="hiddenCB">
+                <div className="hiddenCB">
                     <h3>Search field</h3>
 
                     <div>
-                     
+
                         {
-                            data[0]?
-                            columns.map((elm, idx) => {
-                                return (
-                                    <React.Fragment>
-                                        <input key={idx} type="checkbox" checked={this.state.query_field.includes(elm)} 
+                            this.props.data[0] ?
+                                columns.map((elm, idx) => {
+                                    return (
                                         
-                                            onClick = {(e) => {
-                                                const checked = this.state.query_field.includes(elm)
-                                                const query_field = {...this.state}.query_field
-                                                if(checked)
-                                                {
-                                                    query_field.pop(elm)
-                                                }
-                                                else{
-                                                    query_field.push(elm)
-                                                }
-                                                this.setState({query_field:query_field})
-                                            }}
-                                        /><label >{elm}</label>
-                                    </React.Fragment>
+                                            <label key={idx}>
+                                                <input key={idx} type="checkbox" value="ok" checked={this.state.query_field.includes(elm)}
+                                                    onChange={(e) => {
+                                                        const checked = this.state.query_field.includes(elm)
+                                                        const query_field = { ...this.state }.query_field
+                                                        if (checked) {
+                                                            query_field.splice(query_field.indexOf(elm), 1)
+                                                        }
+                                                        else {
+                                                            query_field.push(elm)
+                                                        }
+                                                        this.setState({ query_field: query_field })
+                                                    }}
 
-                                )
 
-                            }):null
+                                                />
+                                                {elm}
+                                            </label>
+
+                                        
+
+                                    )
+
+                                }) : null
                         }
 
                     </div>
