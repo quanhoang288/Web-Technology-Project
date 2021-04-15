3b73904097309ba6cb4628fd3f90629aaf26376a
thangVjppro <hsgsdominate@gmail.com>
2021-04-15 10:35:56 +0700

    'UpdateQuery'

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,316 +1,339 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: studentmanagement
--- ------------------------------------------------------
--- Server version	8.0.23
+-- Host: 127.0.0.1
+-- Generation Time: Apr 15, 2021 at 05:30 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cnweb`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `course`
 --
 
-DROP TABLE IF EXISTS `course`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `course` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `fee` int NOT NULL,
-  `teacher_id` int NOT NULL,
-  `subject` varchar(45) NOT NULL,
-  `min` int NOT NULL,
-  `max` int NOT NULL,
-  `level` varchar(45) NOT NULL,
-  `cur_amount` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `fee` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `subject` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `min` int(11) NOT NULL,
+  `max` int(11) NOT NULL,
+  `level` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `cur_amount` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `due_registration_date` datetime DEFAULT NULL,
-  `status` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `t_id_idx` (`teacher_id`),
-  CONSTRAINT `t_id` FOREIGN KEY (`teacher_id`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `course`
---
--- ORDER BY:  `id`
-
-LOCK TABLES `course` WRITE;
-/*!40000 ALTER TABLE `course` DISABLE KEYS */;
-/*!40000 ALTER TABLE `course` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `course_notification`
 --
 
-DROP TABLE IF EXISTS `course_notification`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `course_notification` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `content` varchar(45) DEFAULT NULL,
-  `cid` int NOT NULL,
-  `craeteat` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `cid4_idx` (`cid`),
-  CONSTRAINT `cid4` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `id` int(11) NOT NULL,
+  `content` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cid` int(11) NOT NULL,
+  `craeteat` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `course_notification`
---
--- ORDER BY:  `id`
-
-LOCK TABLES `course_notification` WRITE;
-/*!40000 ALTER TABLE `course_notification` DISABLE KEYS */;
-/*!40000 ALTER TABLE `course_notification` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `document`
 --
 
-DROP TABLE IF EXISTS `document`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `document` (
-  `id` int NOT NULL,
-  `cid` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
   `time_created` datetime NOT NULL,
-  `filename` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`,`cid`),
-  KEY `cid6_idx` (`cid`),
-  CONSTRAINT `cid6` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `filename` varchar(45) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `document`
---
--- ORDER BY:  `id`,`cid`
-
-LOCK TABLES `document` WRITE;
-/*!40000 ALTER TABLE `document` DISABLE KEYS */;
-/*!40000 ALTER TABLE `document` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `enrol`
 --
 
-DROP TABLE IF EXISTS `enrol`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `enrol` (
-  `sid` int NOT NULL,
-  `cid` int NOT NULL,
-  PRIMARY KEY (`sid`,`cid`),
-  KEY `cid3_idx` (`cid`),
-  CONSTRAINT `cid3` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `sid3` FOREIGN KEY (`sid`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `sid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `enrol`
---
--- ORDER BY:  `sid`,`cid`
-
-LOCK TABLES `enrol` WRITE;
-/*!40000 ALTER TABLE `enrol` DISABLE KEYS */;
-/*!40000 ALTER TABLE `enrol` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `exam`
 --
 
-DROP TABLE IF EXISTS `exam`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `exam` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `taskname` varchar(45) NOT NULL,
-  `content` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `taskname` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `content` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `cid` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `cid5_idx` (`cid`),
-  CONSTRAINT `cid5` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `cid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `exam`
---
--- ORDER BY:  `id`
-
-LOCK TABLES `exam` WRITE;
-/*!40000 ALTER TABLE `exam` DISABLE KEYS */;
-/*!40000 ALTER TABLE `exam` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `payment`
 --
 
-DROP TABLE IF EXISTS `payment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `payment` (
-  `sid` int NOT NULL,
-  `cid` int NOT NULL,
-  `paid` tinyint(1) NOT NULL,
-  PRIMARY KEY (`sid`,`cid`),
-  KEY `cid_idx` (`cid`),
-  CONSTRAINT `cid` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `sid` FOREIGN KEY (`sid`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `sid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `paid` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `payment`
---
--- ORDER BY:  `sid`,`cid`
-
-LOCK TABLES `payment` WRITE;
-/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `result`
 --
 
-DROP TABLE IF EXISTS `result`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `result` (
-  `eid` int NOT NULL,
-  `sid` int NOT NULL,
-  `score` int DEFAULT NULL,
-  PRIMARY KEY (`eid`,`sid`),
-  KEY `sid5_idx` (`sid`),
-  CONSTRAINT `eid` FOREIGN KEY (`eid`) REFERENCES `exam` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `sid5` FOREIGN KEY (`sid`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `eid` int(11) NOT NULL,
+  `sid` int(11) NOT NULL,
+  `score` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `result`
---
--- ORDER BY:  `eid`,`sid`
-
-LOCK TABLES `result` WRITE;
-/*!40000 ALTER TABLE `result` DISABLE KEYS */;
-/*!40000 ALTER TABLE `result` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `schedule`
 --
 
-DROP TABLE IF EXISTS `schedule`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `schedule` (
-  `cid` int NOT NULL,
-  `weekday_id` int NOT NULL,
-  `time_id` int NOT NULL,
-  PRIMARY KEY (`cid`,`weekday_id`),
-  CONSTRAINT `cid2` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `cid` int(11) NOT NULL,
+  `weekday_id` int(11) NOT NULL,
+  `time_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `schedule`
---
--- ORDER BY:  `cid`,`weekday_id`
-
-LOCK TABLES `schedule` WRITE;
-/*!40000 ALTER TABLE `schedule` DISABLE KEYS */;
-/*!40000 ALTER TABLE `schedule` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `system_notification`
 --
 
-DROP TABLE IF EXISTS `system_notification`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `system_notification` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `content` varchar(45) NOT NULL,
-  `created_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL,
+  `content` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `system_notification`
---
--- ORDER BY:  `id`
-
-LOCK TABLES `system_notification` WRITE;
-/*!40000 ALTER TABLE `system_notification` DISABLE KEYS */;
-/*!40000 ALTER TABLE `system_notification` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `firstname` varchar(45) NOT NULL,
-  `lastname` varchar(45) NOT NULL,
-  `phone` varchar(45) NOT NULL,
-  `school` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `username` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `firstname` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `school` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `active` tinyint(1) NOT NULL,
-  `role` varchar(45) NOT NULL,
-  PRIMARY KEY (`Id`,`Username`)
+  `role` varchar(45) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
--- ORDER BY:  `Id`,`Username`
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `user` (`id`, `username`, `password`, `firstname`, `lastname`, `phone`, `school`, `active`, `role`) VALUES
+(4, 'thangnd', '$2y$10$MiFtWZxdigJzcOXXDEQTMOAdWVJdBuSrTQmmOb35WyTTxefiFqLMa', 'tajhsdg', 'dsaf', '328409328', 'dsfdhsk', 1, 'admin'),
+(5, 'seaniwq234342eordev', '$2y$10$VGSWCcmksc67qSU64GhsKe4qPhkUr7mwoKRbeKdbtGMujok2X.Vc2', 'sfef@gmail.com', 'adawd', 'ASD', '1231', 1, 'ASD'),
+(29, 'thangprovjp', '$2y$10$BaX7I9cQS0MWJcKSkaVGTuuD95CVxkXMY.krtpiG/0NCfmX20L4/W', 'Nguyen', 'Thang', 'Nguyen Du', '0917789338', 1, 's'),
+(45, 'thangprovjpkhack ', '$2y$10$trq600FaRZ3tRiD4raMxSuAhnrbzzewRtugUD/H6GtDqHFR8375Hy', 'Thang', 'Nguyen', 'Nguyen Du', '0921312341', 1, 's'),
+(67, 'thangprovjpkhack1', '$2y$10$YltII41guhqcXpA5YIV4xORenpvFhJxUQFxCiaBO4S6gG04MpX8Oq', 'Thang', 'Nguyen', 'Nguyen Du', '0921312341', 1, 's'),
+(69, 'thangprovjpvkl', '$2y$10$okFoT9.0Jqhq9f5pnUrSw./V6adwdv1HHg2s8o0LRBYf0RxV7Ttj.', '1231231', '123123', 'lksdfjl', '2342342341', 1, 's'),
+(72, 'thangprovjp12', '$2y$10$kbyBCTlBVRaC2XT2TWAGROLCwHXOvECP2icc4Sz491nX7/C1td3kq', 'ídkjhfk', 'jkskjdfh', 'lkdjfhlkj', '21389129083', 1, 's'),
+(73, 'thangabc', '$2y$10$KTXBLeB3zh2onp4iSGlQYOXh1ov9GRzQ3zC0L/C0qgWGKQqUQHDSi', 'Nguyen', 'Thang', 'NguyenDuc', '879327423', 1, 's');
 
 --
--- Dumping routines for database 'studentmanagement'
+-- Indexes for dumped tables
 --
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `t_id_idx` (`teacher_id`);
+
+--
+-- Indexes for table `course_notification`
+--
+ALTER TABLE `course_notification`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cid4_idx` (`cid`);
+
+--
+-- Indexes for table `document`
+--
+ALTER TABLE `document`
+  ADD PRIMARY KEY (`id`,`cid`),
+  ADD KEY `cid6_idx` (`cid`);
+
+--
+-- Indexes for table `enrol`
+--
+ALTER TABLE `enrol`
+  ADD PRIMARY KEY (`sid`,`cid`),
+  ADD KEY `cid3_idx` (`cid`);
+
+--
+-- Indexes for table `exam`
+--
+ALTER TABLE `exam`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cid5_idx` (`cid`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`sid`,`cid`),
+  ADD KEY `cid_idx` (`cid`);
+
+--
+-- Indexes for table `result`
+--
+ALTER TABLE `result`
+  ADD PRIMARY KEY (`eid`,`sid`),
+  ADD KEY `sid5_idx` (`sid`);
+
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`cid`,`weekday_id`);
+
+--
+-- Indexes for table `system_notification`
+--
+ALTER TABLE `system_notification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`,`username`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `course_notification`
+--
+ALTER TABLE `course_notification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `exam`
+--
+ALTER TABLE `exam`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `system_notification`
+--
+ALTER TABLE `system_notification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `course`
+--
+ALTER TABLE `course`
+  ADD CONSTRAINT `t_id` FOREIGN KEY (`teacher_id`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `course_notification`
+--
+ALTER TABLE `course_notification`
+  ADD CONSTRAINT `cid4` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `document`
+--
+ALTER TABLE `document`
+  ADD CONSTRAINT `cid6` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `enrol`
+--
+ALTER TABLE `enrol`
+  ADD CONSTRAINT `cid3` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sid3` FOREIGN KEY (`sid`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `exam`
+--
+ALTER TABLE `exam`
+  ADD CONSTRAINT `cid5` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `cid` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sid` FOREIGN KEY (`sid`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `result`
+--
+ALTER TABLE `result`
+  ADD CONSTRAINT `eid` FOREIGN KEY (`eid`) REFERENCES `exam` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sid5` FOREIGN KEY (`sid`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `cid2` FOREIGN KEY (`cid`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-03-26  0:52:22
