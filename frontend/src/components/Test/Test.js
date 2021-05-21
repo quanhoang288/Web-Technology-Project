8f8d696fca8d91ff387048545ee41cc0f9ae179c
thangnd3629 <thang.nd183629@gmail.com>
2021-05-21 11:45:37 +0700

    Update

--- frontend/src/components/Test/Test.js
+++ frontend/src/components/Test/Test.js
@@ -9,7 +9,65 @@ export class Test extends Component {
     fileName: null,
     file : null
   };
-  
+  <!DOCTYPE html>
+<html>
+<head>
+	<title>Download File using PHP</title>
+</head>
+<body>
+
+<!-- <h2>Download File from HERE : </h2>
+<a href="download.php?file=image.jpg">click HERE</a> -->
+
+
+
+</body>
+</html>
+
+// <?php 
+
+
+// if (isset($_SERVER['HTTP_ORIGIN'])) {
+//     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+//     header('Access-Control-Allow-Credentials: true');
+//     header('Access-Control-Max-Age: 86400');    // cache for 1 day
+// }
+
+// // Access-Control headers are received during OPTIONS requests
+// if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+//     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+//         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");         
+
+//     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+//         header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+//     exit(0);
+// }
+
+// if(!empty($_GET['file']))
+// {
+// 	$filename = basename($_GET['file']);
+// 	$filepath = 'upload/' . $filename;
+// 	if(!empty($filename) && file_exists($filepath)){
+
+// //Define Headers
+// 		header("Cache-Control: public");
+// 		header("Content-Description: FIle Transfer");
+// 		header("Content-Disposition: attachment; filename=$filename");
+// 		header("Content-Type: application/zip");
+// 		header("Content-Transfer-Emcoding: binary");
+
+// 		readfile($filepath);
+// 		exit;
+
+// 	}
+// 	else{
+// 		echo "This File Does not exist.";
+// 	}
+// }
+
+//  ?>
   downloadRequest = () => {
     
     // axios({
